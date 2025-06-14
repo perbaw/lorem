0039ce798be0e6891e5bfcad905b45fa023253c4
perbaw <prabhav.shetty@research.iiit.ac.in>
2025-06-14 07:22:41 +0530

    added text and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{Parser, ValueEnum};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+struct Args {
+    unit: Unit,
+    count: usize,
+}
+
+enum Unit {
+    Words,
+    Sentences,
+    Paragraphs,
+    Bytes
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
